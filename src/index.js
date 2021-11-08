e5c7095ec5d02b1343f12c0cd81273dd6c986eca
Michael Lobman <michael.lobman@gmail.com>
2021-11-08 12:44:08 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset();
+  })
 });
+
+function buildToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  li.textContent = `${todo} `;
+  document.querySelector('ul').append(li);
+  li.append(btn);
+  btn.addEventListener('click', removeTask);
+
+}
+
+function removeTask(e) {
+  e.target.parentNode.remove();
+}
+
+
